96a6f558724f13b058409bd1b0daf5e5ffbb4fa9
Sanket Khardekar <khardekarsanket@gmail.com>
2022-01-15 15:58:12 +0530

    Created GoalItem Component to display single goal.

---
+++ src/components/Goals/GoalItem/GoalItem.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./GoalItem.module.css";
+
+const GoalItem = (props) => {
+  const deleteHandler = () => {
+    props.onDelete(props.id);
+  };
+  return (
+    <li className={styles.goal - item} onClick={deleteHandler}>
+      {props.children}
+    </li>
+  );
+};
+export default GoalItem;
